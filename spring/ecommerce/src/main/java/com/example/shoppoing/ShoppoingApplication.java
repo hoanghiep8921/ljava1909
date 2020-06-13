eaeaffc50a911cf86445620d43ff93d0c6e305f0
hoanghiep8921 <hoanghiep8921@gmail.com>
2020-06-13 20:09:16 +0700

    update

---
+++ spring/ecommerce/src/main/java/com/example/shoppoing/ShoppoingApplication.java
@@ -0,0 +1,13 @@
+package com.example.shoppoing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShoppoingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShoppoingApplication.class, args);
+    }
+
+}
---
+++ spring/ecommerce/src/test/java/com/example/shoppoing/ShoppoingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.shoppoing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShoppoingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
